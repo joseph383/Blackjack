391da2a59fcd9ea3c985c75b088eb33a4d57bfe0
Joseph Ancona <josephaancona@gmail.com>
2021-03-03 16:35:32 -0500

    Add View class

---
+++ View/View.java
@@ -0,0 +1,8 @@
+package view;
+
+public class View {
+
+	public static void startupGame() {
+		System.out.println("Welcome to Command Line BlackJack!!! This text-based game will simulate the classic card game without a user interface.\n");
+	}
+}
